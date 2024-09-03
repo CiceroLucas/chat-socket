924fabcab6046906e379738aef55dc73aa33d3fa
Italo souza sampaio <italo12346@gmail.com>
2024-09-03 14:15:32 -0300

    fix: correção de bugs

--- client.py
+++ client.py
@@ -1,38 +1,36 @@
 import socket
 import threading
 
-def receive_messages(client_socket):
+# Configurações do cliente
+HOST = '127.0.0.1'  # Deve ser o mesmo do servidor
+PORT = 5000         # Deve ser o mesmo do servidor
+
+def receive_messages(client):
     while True:
         try:
-            message = client_socket.recv(1024).decode('utf-8')
-            if message:
-                print(message)
+            message = client.recv(1024).decode('utf-8')
+            if message == 'NOME':
+                client.send(username.encode('utf-8'))
             else:
-                break
+                print(message)
         except:
+            print("Ocorreu um erro ao receber a mensagem.")
+            client.close()
             break
 
-def start_client(host='127.0.0.1', port=12345):
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client_socket.connect((host, port))
-    
-    # Enviar nickname
-    nickname = input("Enter your nickname: ")
-    client_socket.send(nickname.encode('utf-8'))
-
-    # Iniciar thread para receber mensagens
-    receive_thread = threading.Thread(target=receive_messages, args=(client_socket,))
-    receive_thread.start()
-
+def send_messages(client):
     while True:
-        try:
-            message = input()
-            if message:
-                client_socket.send(message.encode('utf-8'))
-        except KeyboardInterrupt:
-            break
-
-    client_socket.close()
+        message = input()
+        client.send(message.encode('utf-8'))
 
 if __name__ == "__main__":
-    start_client()
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect((HOST, PORT))
+
+    username = input("Digite seu nome de usuário: ")
+
+    receive_thread = threading.Thread(target=receive_messages, args=(client,))
+    receive_thread.start()
+
+    send_thread = threading.Thread(target=send_messages, args=(client,))
+    send_thread.start()
